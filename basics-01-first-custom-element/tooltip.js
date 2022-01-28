a2ae628c7e5d19a457cd452bab0bc7dbadf04d7d
Willy Alers <willy.alers@allheart.net>
2022-01-28 12:31:25 -0800

    28 Using Attributes on Custom Elements.

--- basics-01-first-custom-element/tooltip.js
+++ basics-01-first-custom-element/tooltip.js
@@ -6,17 +6,21 @@ class Tooltip extends HTMLElement {
 
     //this is a built in function you need to use for DOM manupulation
     connectedCallback(){
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip);
-        tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
 
         this.appendChild(tooltipIcon);
     }
 
     _showTooltip(){
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
+        this._tooltipContainer.textContent = this._tooltipText;
         this.appendChild(this._tooltipContainer);
     }
 
