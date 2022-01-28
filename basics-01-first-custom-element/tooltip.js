500c7fca4d21c6b7a10194f9f361004050943154
Willy Alers <willy.alers@allheart.net>
2022-01-28 12:08:47 -0800

    27 Listening to Events Inside Components.

--- basics-01-first-custom-element/tooltip.js
+++ basics-01-first-custom-element/tooltip.js
@@ -1,13 +1,27 @@
 class Tooltip extends HTMLElement {
     constructor(){
         super();
+        this._tooltipContainer;
     }
 
+    //this is a built in function you need to use for DOM manupulation
     connectedCallback(){
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip);
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
 
         this.appendChild(tooltipIcon);
+    }
+
+    _showTooltip(){
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = 'This is the tooltip text!';
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip(){
+        this.removeChild(this._tooltipContainer);
     }
 }
 
