256b189a06ce3c9f02b405993833474ec6fab37d
Willy Alers <willy.alers@allheart.net>
2022-01-31 14:08:08 -0800

    65 Closing the Modal with the Modal Buttons.

--- complex-cmp-01-starting-setup/modal.js
+++ complex-cmp-01-starting-setup/modal.js
@@ -75,8 +75,8 @@ class Modal extends HTMLElement{
                     <slot></slot>
                 </section>
                 <section id="actions">
-                    <button>Cancel</button>
-                    <button>Okay</button>
+                    <button id="cancel-btn">Cancel</button>
+                    <button id="confirm-btn">Okay</button>
                 </section>
             </div>
         `;
@@ -86,6 +86,12 @@ class Modal extends HTMLElement{
             console.dir(event);
             console.dir(slots[1].assignedNodes());
         });
+
+        const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+        const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+
+        cancelButton.addEventListener('click', this._cancel.bind(this));
+        confirmButton.addEventListener('click', this._confirm.bind(this));
     }
 
     // attributeChangedCallback(name, oldValue, newValue) {
@@ -106,6 +112,20 @@ class Modal extends HTMLElement{
 
     open(){
         this.setAttribute('opened','');
+    }
+
+    hide(){
+        if (this.hasAttribute('opened')){
+            this.removeAttribute('opened');
+        }
+    }
+
+    _cancel(){
+        this.hide();
+    }
+
+    _confirm(){
+        this.hide();
     }
 }
 
