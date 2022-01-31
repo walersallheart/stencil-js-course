266f59c8a7c5588a0c7172646e4220a82607fa5c
Willy Alers <willy.alers@allheart.net>
2022-01-31 12:37:52 -0800

    57 Creating the Basics Modal Component.

---
+++ complex-cmp-01-starting-setup/modal.js
@@ -0,0 +1,28 @@
+class Modal extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode:'open'});
+        this.shadowRoot.innerHTML = `
+            <style>
+                #backdrop{
+                    position:fixed;
+                    top:0;
+                    left:0;
+                    width:100%;
+                    height:100vh;
+                    background:rgba(0,0,0,.75);
+                }
+
+                #modal{
+                    
+                }
+            </style>
+
+            <div id="backdrop"></div>
+            <div id="modal"></div>
+        `;
+    }
+}
+
+customElements.define('uc-modal', Modal);
