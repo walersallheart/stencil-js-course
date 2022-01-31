b9c1ea66b0c2f1ff16555d3e5d9eb64db6272f26
Willy Alers <willy.alers@allheart.net>
2022-01-31 12:46:27 -0800

    59 Styling the Modal Elements.

--- complex-cmp-01-starting-setup/modal.js
+++ complex-cmp-01-starting-setup/modal.js
@@ -20,16 +20,52 @@ class Modal extends HTMLElement{
                     top:15vh;
                     left:25%;
                     width:50%;
-                    height:30rem;
                     z-index:100;
                     background:white;
                     border-radius:3px;
                     box-shadow:0 2px 8px rgba(0,0,0,0.26);
+                    display:flex;
+                    flex-direction:column;
+                    justify-content:space-between;
+                }
+
+                header{
+                    padding:1rem;
+                }
+
+                header h1{
+                    font-size:1.25rem;
+                }
+
+                #main{
+                    padding:1rem;
+                }
+
+                #actions{
+                    border-top:1px solid #ccc;
+                    padding:1rem;
+                    display:flex;
+                    justify-content:flex-end;
+                }
+
+                #actions button{
+                    margin:0 .25rem;
                 }
             </style>
 
             <div id="backdrop"></div>
-            <div id="modal"></div>
+            <div id="modal">
+                <header>
+                    <h1>Please Confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>Okay</button>
+                </section>
+            </div>
         `;
     }
 }
