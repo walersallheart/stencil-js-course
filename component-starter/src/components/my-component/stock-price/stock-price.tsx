d6ea0faecc339fd77844bee444f70c9022a82516
Willy Alers <willy.alers@allheart.net>
2022-02-02 14:12:07 -0800

    105 HTTP Requests in Components.

--- component-starter/src/components/my-component/stock-price/stock-price.tsx
+++ component-starter/src/components/my-component/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component } from "@stencil/core";
+import { Component, State } from "@stencil/core";
 import { API_KEY } from '../../../config/config.js';
 
 @Component({
@@ -8,20 +8,32 @@ import { API_KEY } from '../../../config/config.js';
 })
 
 export class StockPrice{
+    @State() fetchedPrice:number;
+
     onFetchStockPrice(event:Event){
         event.preventDefault();
         console.log('submitted');
         console.log(API_KEY);
+
+        fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo')
+            .then(response => {
+                return response.json()
+            })
+            .then(parsedResponse => {
+                console.log(parsedResponse);
+                this.fetchedPrice = +parsedResponse['Global Quote']['05. price'];
+            })
+            .catch(err => console.log(err));
     }
 
     render(){
         return [
-            <form onSubmit={this.onFetchStockPrice}>
+            <form onSubmit={this.onFetchStockPrice.bind(this)}>
                 <input id="stock-symbol" />
                 <button type="submit">Fetch</button>
             </form>,
             <div>
-                <p>Price: {0}</p>
+                <p>Price: ${this.fetchedPrice}</p>
             </div>
         ];
     }
