474b5a2d5602535fc3f8797080b8ff23778657b9
Willy Alers <willy.alers@allheart.net>
2022-02-02 12:52:51 -0800

    94 Adding More Event Listeners.

--- component-starter/src/components/my-component/side-drawer/side-drawer.tsx
+++ component-starter/src/components/my-component/side-drawer/side-drawer.tsx
@@ -14,7 +14,24 @@ export class SideDrawer {
         this.open = false;
     }
 
+    onContentChange(content:string){
+        console.log(content);
+    }
+
     render(){
+        let mainContent = <slot/>;
+
+        mainContent =(
+            <div id="contact-information">
+                <h2>Contact Information</h2>
+                <p>You can reach us  via phone or email.</p>
+                <ul>
+                    <li>Phone: 123-456-7890</li>
+                    <li>E-Mail: <a href="mailto:something@something.com">something@something.com</a></li>
+                </ul>
+            </div>
+        )
+
         return (
             <aside>
                 <header>
@@ -22,11 +39,11 @@ export class SideDrawer {
                     <button onClick={this.onCloseDrawer.bind(this)}>X</button>
                 </header>
                 <section id="tabs">
-                    <button class="active">Navigation</button>
-                    <button>Content</button>
+                    <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+                    <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
                 </section>
                 <main>
-                    <slot />
+                    {mainContent}
                 </main>
             </aside>
         )
