9f54e4decd019066894c8db2abfcd946510b4ee9
Willy Alers <willy.alers@allheart.net>
2022-02-02 11:40:33 -0800

    83 Styling a Stencil Component.

--- component-starter/src/components.d.ts
+++ component-starter/src/components.d.ts
@@ -12,25 +12,32 @@ import '@stencil/core';
 
 export namespace Components {
 
+  interface UcSideDrawer {}
+  interface UcSideDrawerAttributes extends StencilHTMLAttributes {}
 }
 
 declare global {
   interface StencilElementInterfaces {
-
+    'UcSideDrawer': Components.UcSideDrawer;
   }
 
   interface StencilIntrinsicElements {
-
+    'uc-side-drawer': Components.UcSideDrawerAttributes;
   }
 
 
+  interface HTMLUcSideDrawerElement extends Components.UcSideDrawer, HTMLStencilElement {}
+  var HTMLUcSideDrawerElement: {
+    prototype: HTMLUcSideDrawerElement;
+    new (): HTMLUcSideDrawerElement;
+  };
 
   interface HTMLElementTagNameMap {
-
+    'uc-side-drawer': HTMLUcSideDrawerElement
   }
 
   interface ElementTagNameMap {
-
+    'uc-side-drawer': HTMLUcSideDrawerElement;
   }
 
 
---
+++ component-starter/src/components/my-component/side-drawer/side-drawer.tsx
@@ -0,0 +1,15 @@
+import { Component } from "@stencil/core";
+
+@Component({
+    tag:'uc-side-drawer'
+})
+
+export class SideDrawer {
+    render(){
+        return (
+            <div>
+                <h1>The Side Drawer</h1>
+            </div>
+        )
+    }
+}
