6d23c27ffdc334fab025d7e489f485f6872abfe1
Willy Alers <willy.alers@allheart.net>
2022-02-04 10:55:42 -0800

    131 Building & Using Components.

--- component-starter/stencil.config.ts
+++ component-starter/stencil.config.ts
@@ -1,13 +1,15 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
+  namespace: 'UdemyWKCourse',
   outputTargets:[
-    { type: 'dist' },
-    { type: 'docs' },
     {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
+        type: 'dist'
+    },
+    //{ type: 'docs' },
+    // {
+    //   type: 'www',
+    //   serviceWorker: null // disable service workers
+    // }
   ]
 };
